1728a6af5454a67f4931d807ed15f0c97f11c8ac
Jacob Allen <jedijake@me.com>
2016-08-24 09:56:18 -0700

    add elevator effect to all links that direct to a location on the site

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,15 @@
   console.log("js loaded");
 
   // ELEVATOR JS VARIABLES AND FUNCTIONS
-  var projects = document.getElementById("projectsLink");
-  var aboutMe = document.getElementById("aboutMeLink");
-  var contact = document.getElementById("contactLink");
-  var aboutuslink = document.getElementById("aboutuslink");
+  var projects         = document.getElementById("projectsLink");
+  var aboutMe          = document.getElementById("aboutMeLink");
+  var contact          = document.getElementById("contactLink");
+  var projectsAboutMe  = document.getElementById("projectsAboutMe");
+  var projectsContact  = document.getElementById("projectsContact");
+  var aboutMeProjects  = document.getElementById("aboutMeProjects");
+  var aboutMeContact   = document.getElementById("aboutMeContact");
+  var contactProjects  = document.getElementById("contactProjects");
+  var contactAboutMe   = document.getElementById("contactAboutMe");
   new Elevator({
     element: projects,
     targetElement: document.querySelector("#projects")
@@ -19,7 +24,27 @@
     targetElement: document.querySelector("#contact")
     });
   new Elevator({
-    element: aboutuslink,
-    targetElement: document.querySelector("#aboutus")
+    element: projectsAboutMe,
+    targetElement: document.querySelector("#aboutMe")
+    });
+  new Elevator({
+    element: projectsContact,
+    targetElement: document.querySelector("#contact")
+    });
+  new Elevator({
+    element: aboutMeProjects,
+    targetElement: document.querySelector("#projects")
+    });
+  new Elevator({
+    element: aboutMeContact,
+    targetElement: document.querySelector("#contact")
+    });
+  new Elevator({
+    element: contactProjects,
+    targetElement: document.querySelector("#projects")
+    });
+  new Elevator({
+    element: contactAboutMe,
+    targetElement: document.querySelector("#aboutMe")
     });
 })();
