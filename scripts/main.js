3dcf83393835d08818e7efba74fa81bb727612ad
Jacob Allen <jedijake@me.com>
2016-08-24 09:46:30 -0700

    add elevator effect to projects, aboutMe, and contact links at top of site

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-console.log("js loaded");
+(function() {
+  console.log("js loaded");
+
+  // ELEVATOR JS VARIABLES AND FUNCTIONS
+  var projects = document.getElementById("projectsLink");
+  var aboutMe = document.getElementById("aboutMeLink");
+  var contact = document.getElementById("contactLink");
+  var aboutuslink = document.getElementById("aboutuslink");
+  new Elevator({
+    element: projects,
+    targetElement: document.querySelector("#projects")
+    });
+  new Elevator({
+    element: aboutMe,
+    targetElement: document.querySelector("#aboutMe")
+    });
+  new Elevator({
+    element: contact,
+    targetElement: document.querySelector("#contact")
+    });
+  new Elevator({
+    element: aboutuslink,
+    targetElement: document.querySelector("#aboutus")
+    });
+})();
